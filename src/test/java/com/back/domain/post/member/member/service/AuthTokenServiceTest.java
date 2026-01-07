9d76fa07fb1bc6bcba0b844cbc5e24252f2d8d55
zunobk <junho4560@naver.com>
2026-01-07 14:11:16 +0900

    JWT 생성 테스트

--- src/test/java/com/back/domain/post/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/back/domain/post/member/member/service/AuthTokenServiceTest.java
@@ -1,12 +1,19 @@
 package com.back.domain.post.member.member.service;
 
 import com.back.domain.member.member.service.AuthTokenService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +28,30 @@ public class AuthTokenServiceTest {
     @DisplayName("authTokenService 서비스가 존재한다.")
     void t1() {
         assertThat(authTokenService).isNotNull();
+    }
+
+    @Test
+    @DisplayName("jjwt 최신 방식으로 JWT 생성, {name=\"Paul\", age=23}")
+    void t2() {
+        // 토큰 만료기간: 1년
+        long expireMillis = 1000L * 60 * 60 * 24 * 365;
+
+        byte[] keyBytes = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes(StandardCharsets.UTF_8);
+        SecretKey secretKey = Keys.hmacShaKeyFor(keyBytes);
+
+        // 발행 시간과 만료 시간 설정
+        Date issuedAt = new Date();
+        Date expiration = new Date(issuedAt.getTime() + expireMillis);
+
+        String jwt = Jwts.builder()
+                .claims(Map.of("name", "Paul", "age", 23)) // 내용
+                .issuedAt(issuedAt) // 생성날짜
+                .expiration(expiration) // 만료날짜
+                .signWith(secretKey) // 키 서명
+                .compact();
+
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt = " + jwt);
     }
 }
