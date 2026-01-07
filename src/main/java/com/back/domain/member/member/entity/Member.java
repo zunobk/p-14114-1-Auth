1c41723b58ea67a8c977b0b7460ebdf175f260e9
zunobk <junho4560@naver.com>
2026-01-07 09:47:42 +0900

    샘플 회원 5명 만큼은 apiKey와 username을 동일하게 하여 개발편의성을 높힌다.

--- src/main/java/com/back/domain/member/member/entity/Member.java
+++ src/main/java/com/back/domain/member/member/entity/Member.java
@@ -29,4 +29,8 @@ public class Member extends BaseEntity {
     public String getName() {
         return nickname;
     }
+
+    public void modifyApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
 }
