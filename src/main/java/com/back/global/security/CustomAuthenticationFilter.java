2a390df729be599855aa9930b6a20802caf3addb
zunobk <junho4560@naver.com>
2026-01-09 14:48:52 +0900

    실험 2 : filterChain.doFilter(request, response); 를 주석

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -16,14 +16,5 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         logger.debug("CustomAuthenticationFilter: Processing request for " + request.getRequestURI());
-
-        if (request.getRequestURI().startsWith("/api/")) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write("{\"resultCode\": \"404-1\", \"msg\": \"메롱\"}");
-            return;
-        }
-
-        filterChain.doFilter(request, response);
     }
 }
