1e7aab56464043d259433ff705e7e38a2aa361a1
zunobk <junho4560@naver.com>
2026-01-09 10:13:52 +0900

    GET /api/v1/adm/members - 회원 다건조회 구현

---
+++ src/main/java/com/back/domain/member/member/controller/ApiV1AdmMemberController.java
@@ -0,0 +1,32 @@
+package com.back.domain.member.member.controller;
+
+
+import com.back.domain.member.member.dto.MemberDto;
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.service.MemberService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/adm/members")
+@RequiredArgsConstructor
+@Tag(name = "ApiV1AdmMemberController", description = "관리자용 API 회원 컨트롤러")
+@SecurityRequirement(name = "bearerAuth")
+public class ApiV1AdmMemberController {
+    private final MemberService memberService;
+
+    @GetMapping
+    public List<MemberDto> getItems() {
+        List<Member> members = memberService.findAll();
+
+        return members.stream()
+                .map(MemberDto::new)
+                .toList();
+    }
+}
--- src/main/java/com/back/domain/member/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -6,6 +6,7 @@ import com.back.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -49,5 +50,9 @@ public class MemberService {
 
     public Optional<Member> findById(int id) {
         return memberRepository.findById(id);
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
     }
 }
