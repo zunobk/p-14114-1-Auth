156ada91a8b114cffba150beddf4944b0c49bb8f
zunobk <junho4560@naver.com>
2026-01-09 15:24:22 +0900

    인증/인가 필요없는 요청이라면 패스 처리, apiKey, accessToken 얻기 커밋한개 실수해서 추가함

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.back.global.security;
 
+import com.back.global.exception.ServiceException;
+import com.back.global.rq.Rq;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,13 +11,49 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
+    private final Rq rq;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        logger.debug("CustomAuthenticationFilter: Processing request for " + request.getRequestURI());
+        logger.debug("Processing request for " + request.getRequestURI());
+
+        // API 요청이 아니라면 패스
+        if (!request.getRequestURI().startsWith("/api/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 인증, 인가가 필요없는 API 요청이라면 패스
+        if (List.of("/api/v1/members/login", "/api/v1/members/logout", "/api/v1/members/join").contains(request.getRequestURI())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String apiKey;
+        String accessToken;
+
+        String headerAuthorization = rq.getHeader("Authorization", "");
+
+        if (!headerAuthorization.isBlank()) {
+            if (!headerAuthorization.startsWith("Bearer "))
+                throw new ServiceException("401-2", "Authorization 헤더가 Bearer 형식이 아닙니다.");
+
+            String[] headerAuthorizationBits = headerAuthorization.split(" ", 3);
+
+            apiKey = headerAuthorizationBits[1];
+            accessToken = headerAuthorizationBits.length == 3 ? headerAuthorizationBits[2] : "";
+        } else {
+            apiKey = rq.getCookieValue("apiKey", "");
+            accessToken = rq.getCookieValue("accessToken", "");
+        }
+
+        logger.debug("apiKey : " + apiKey);
+        logger.debug("accessToken : " + accessToken);
 
         filterChain.doFilter(request, response);
     }
