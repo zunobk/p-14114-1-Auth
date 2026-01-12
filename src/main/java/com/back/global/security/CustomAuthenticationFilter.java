3748980041a75752f4adb59dc4e79942014e563c
zunobk <junho4560@naver.com>
2026-01-12 11:21:09 +0900

    시큐리티가 인가를 잘 수행할 수 있도록 요청자가 관리자인지 아닌지 알려준다.

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -12,12 +12,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -119,12 +122,15 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
             rq.setHeader("Authorization", actorAccessToken);
         }
 
+        Collection<? extends GrantedAuthority> authorities = member.isAdmin() ?
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN")) : List.of();
+
         UserDetails user = new SecurityUser(
                 member.getId(),
                 member.getUsername(),
                 "",
                 member.getName(),
-                List.of()
+                authorities
         );
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(
